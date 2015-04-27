3620ec94b45956a9124d160976c15b83aeeac79a
Brian Kropff & Tom Mertz <student@epicodus.com>
2015-04-27 12:11:14 -0700

    Add enemy and inventory expressions

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ app.controller('HeroesCtrl', function() {
     var welcome = this;
 
     welcome.message = "Hello, Mighty Warriors!";
+
+    welcome.dealDamage = function(ability) {
+        alert("You dealt damage! " + ability);
+    };
 });
 
 app.directive("fireball", function() {
@@ -48,7 +52,12 @@ app.directive("myGreeting", function() {
 app.directive("phuz", function() {
     return {
         restrict: "E",
-        template: "<div> I am Phuz, master of the mind!!!</div>"
+        scope: {
+            damage: "&"
+        },
+        template: '<input type="text" ng-model="ability">' +
+        '{{ability}}' +
+        '<button ng-click="damage({ability: ability})">Deal Damage</button>'
     };
 });
 
@@ -56,5 +65,57 @@ app.directive("meatball", function() {
     return {
         restrict: "E",
         template: "<div>Meatball smash!!!</div>"
+    };
+});
+
+app.directive("spyro", function() {
+    return{
+        restrict: "A",
+        link: function () {
+            console.log("Spyro::DOUBLE ROUNDHOUSE-KICK TO THE FACE!!");
+        }
+    };
+});
+
+//****************************//****************************//****************************
+
+app.controller('InventoryCtrl', function() {
+    var inventoryCtrl = this;
+
+    inventoryCtrl.logItem = function(item) {
+        alert("You've added the item " + item);
+    };
+});
+
+app.directive("inventory", function() {
+    return {
+        restrict: "E",
+        scope: {
+            add: "&"
+        },
+        template: '<input type="text" ng-model="item">' +
+        ' {{item}}' +
+        ' <button ng-click="add({item: item})">Add Item</button>'
+    }
+})
+//****************************//****************************//****************************
+
+app.controller('EnemyCtrl', function() {
+    var enemyCtrl = this;
+
+    enemyCtrl.logEnemy = function() {
+        alert("You have encountered an enemy");
+    };
+});
+
+app.directive("enemy", function() {
+    return {
+        restrict: "E",
+        scope: {
+            add: "&"
+        },
+        template: '<input type="text" ng-model="enemy">' +
+        ' {{enemy}}' +
+        '<button ng-click="add({})">Search</button>'
     };
 });
