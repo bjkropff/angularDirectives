25a4789e1c002465a97c9412dc269711f4db1f65
Brian Kropff & Tom Mertz <student@epicodus.com>
2015-04-27 15:56:11 -0700

    Add debug directive

--- test/js/app.js
+++ test/js/app.js
@@ -16,3 +16,48 @@ app.directive("phone", function() {
         '<div class="button" ng-click="dial({message:value})">Call home!</div>'
     };
 });
+
+app.controller("DrinkCtrl", function() {
+    var drinkctrl = this;
+    drinkctrl.ctrlFlavor = "bluberry";
+});
+
+app.directive("drink", function() {
+    return {
+        scope: {
+            flavor: "@"
+        },
+        template: '<div>{{flavor}}</div>'
+    };
+});
+
+app.controller("IceCreamCtrl", function() {
+    var icecream = this;
+
+    icecream.ctrlFlavor = "Raspberry";
+    icecream.flavors = ['Chocolate', 'Vanilla', 'Strawberry', 'Seaweed', 'Blueberry'];
+
+});
+
+app.directive("icecream", function() {
+    return {
+        scope: {
+            flavor: "="
+        },
+        template: '<input type="text" ng-model="flavor">'
+    };
+});
+
+app.directive("debug", function($compile) {
+    return {
+        terminal: true,
+        priority: 1000000,
+        link: function(scope, element) {
+            var clone = element.clone();
+            element.attr("style", "color: red");
+            clone.removeAttr("debug");
+            var clonedElement = $compile(clone)(scope);
+            element.after(clonedElement);
+        }
+    }
+})
